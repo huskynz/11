866da05bb679fc61c53f27507c0c81f9f60ef712
Peter Gallwas <peter@husky.nz>
2025-10-11 21:26:02 +1300

    Readme

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 import Hero from "@/components/Hero";
 import About from "@/components/About";
-import Timeline from "@/components/Timeline";
 import Contact from "@/components/Contact";
 import Socials from "@/components/Socials";
-import Link from "next/link";
-
 export default function Home() {
   return (
     <div className="flex-1 flex flex-col">
