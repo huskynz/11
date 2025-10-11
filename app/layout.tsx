cda73b7afe155717e366790dfcb7a0f1eb6f6fae
Peter Gallwas <peter@husky.nz>
2025-10-11 17:46:58 +1300

    Timeline changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+
 import type { Metadata } from "next";
 import "./globals.css";
 import ClientNavWrapper from "@/components/ClientNavWrapper";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "HuskyNZ | Peter Gallwas",
@@ -14,12 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">
+      <body className="antialiased min-h-screen flex flex-col">
         {/* Navigation Bar */}
-        <div className="pb-16">{/* Add padding to prevent content under nav */}
+  <div className="flex-1 flex flex-col">
           <ClientNavWrapper />
           {children}
         </div>
+        <Footer />
       </body>
     </html>
   );
