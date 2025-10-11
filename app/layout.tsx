d3919c3d48df383c4384002ee44010c5d98be8db
Liam <someaccountmaybe@proton.me>
2025-10-11 23:08:05 +1300

    Adds Grid Background

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import ClientNavWrapper from "@/components/ClientNavWrapper";
 import Footer from "@/components/Footer";
+import GridScrollBG from "@/components/GridScrollBG";
 
 export const metadata: Metadata = {
   title: "HuskyNZ | Peter Gallwas",
@@ -17,8 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased min-h-screen flex flex-col">
+        <GridScrollBG/>
         {/* Navigation Bar */}
-  <div className="flex-1 flex flex-col">
+        <div className="flex-1 flex flex-col">
           <ClientNavWrapper />
           {children}
         </div>
