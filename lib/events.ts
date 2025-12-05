56574df6ac98e2971a2ee37e9e751018885e24ae
Peter Gallwas <peter@husky.nz>
2025-12-05 22:29:28 +1300

    Upgrade NextJS + small wording changes on the timeline

---
+++ lib/events.ts
@@ -0,0 +1,91 @@
+export const timeline = [
+  {
+    year: "2018",
+    events: ["Deployed first Ubuntu 18.04 VM in Hyper-V"],
+    category: 'infrastructure'
+  },
+  {
+    year: "2019",
+    events: ["Continued experimenting with Hyper-V"],
+    category: 'career'
+  },
+  {
+    year: "2020",
+    events: [
+      "Started learning VMware Workstation Pro",
+      "Deployed first Azure Web App",
+      "Created www.husky.nz",
+      "Created my Github account",
+      "Started learning Linux",
+      "Developed skills with Azure",
+    ],
+    category: 'technology'
+  },
+  {
+    year: "2021",
+    events: [
+      "Earned AZ-900 Certification",
+      "Learned more HTML and the web",
+      "Deployed my first Active Directory domain on an Azure VM",
+      "Configured Azure AD Connect to synchronize on-premises Active Directory with Azure Active Directory",
+      "Remote Desktop Deployment with web access and MFA in Azure",
+      "Contributed to development of a Azure Site to site vpn and Azure multi-network WAN",
+      "Developed skills with the Microsoft 365 suite",
+      "Developed skills with Intune and Windows Autopilot",
+    ],
+    category: 'certification'
+  },
+  {
+    year: "2022",
+    events: [
+      "Expanded knowledge of Git and GitHub",
+      "Studied .NET and .NET websites",
+      "Began learning AWS",
+      "Migrated AD Domain from Azure to AWS",
+    ],
+    category: 'technology'
+  },
+  {
+    year: "2023",
+    events: [
+      "Earned MS-900 Certification",
+      "Gained work experience at Inde Technology (5 weeks)",
+      "Began experimenting with Sophos Firewalls",
+      "Started learning Node.js and TypeScript",
+      "Began to learn more about Cloudflare",
+    ],
+    category: 'certification'
+  },
+  {
+    year: "2024",
+    events: [
+      "Used Azure migrate to move AD domain from AWS to Azure",
+      "New Zealand Certificate in Information Technology Essentials (Level 4) at Ara",
+      "Started streaming on Twitch",
+      "Deployed a new version of www.husky.nz using TypeScript and Astro.js (This site)",
+      "Had some fun developing https://legacy.husky.nz witch is a website built with HTML3.0 no CSS and no JavaScript",
+      "Bought a HP DL380 G8 server",
+      "ESXI + Vcenter 8",
+      "Started getting the ground for hnz.li a url shortner built because I was annoyed",
+    ],
+    category: 'project'
+  },
+  {
+    year: "2025",
+    events: [
+      "HP remote management",
+      "Setup Microsoft Configuration Manager in my homelab",
+      "Setup A SAN in my homelab to Learn and Understand network storage",
+      "Setup Citrix Virtual Apps and Desktop and intrgrated with ESXi and Vcenter to auto provisioning and deployment of VDI vm's",
+      "Deployed a more advanced Citrix lab with Netscaler, FAS and ADFS for SAML authentication",
+    ],
+    category: 'infrastructure'
+  },
+  {
+    year: "2026",
+    events: [
+      "More to come...",
+    ],
+    category: 'career'
+  },
+];
