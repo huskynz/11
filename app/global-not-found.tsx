4eb387633a001ca66a550bf463e4f4da8d219830
Peter Gallwas <peter@husky.nz>
2025-12-08 21:03:02 +1300

    Small 404 change

--- app/global-not-found.tsx
+++ app/global-not-found.tsx
@@ -1,24 +1,22 @@
-// Import global styles and fonts
-import './globals.css'
-import { NotFound } from '../components/404'
-import { Inter } from 'next/font/google'
-import type { Metadata } from 'next'
+import "./globals.css";
+import type { Metadata } from "next";
+import GridScrollBG from "@/components/GridScrollBG";
 import Footer from "@/components/Footer";
-import Nav from '@/components/Nav';
- 
-const inter = Inter({ subsets: ['latin'] })
- 
+import { NotFound } from "@/components/404";
+
 export const metadata: Metadata = {
-  title: '404 - Page Not Found',
-  description: 'The page you are looking for does not exist.',
-}
- 
+  title: "404 - Page Not Found",
+  description: "The page you are looking for does not exist.",
+};
+
 export default function GlobalNotFound() {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Nav />
-        <NotFound />
+      <body className="antialiased min-h-screen flex flex-col">
+        <GridScrollBG />
+        <div className="flex-1 flex flex-col">
+          <NotFound />
+        </div>
         <Footer />
       </body>
     </html>
