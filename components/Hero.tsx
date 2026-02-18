d816d0a271e7c3e439b703997653d43c5ddf42d8
Liam <git@liamsherwin.com>
2026-02-18 21:34:25 +1300

    Fixed "Home" Nav link

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-  <section className="relative min-h-screen flex items-center justify-center">
+  <section className="relative min-h-screen flex items-center justify-center" id="home">
       <div className="absolute inset-0 bg-grid-pattern opacity-10"></div>
       <div className="relative z-10 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
         <div className="mb-8">
